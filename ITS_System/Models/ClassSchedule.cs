791e02b3b062819997829198101a3b79688d867c
hobbitboy22 <hobbitboy22@gmail.com>
2023-05-16 11:19:26 +0100

    Add project files.

---
+++ ITS_System/Models/ClassSchedule.cs
@@ -0,0 +1,37 @@
+﻿using FlexAppealFitness.Models;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static ITS_System.Models.Enums;
+
+namespace ITS_System.Models
+{
+    public class ClassSchedule
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        public IdentityUser Instructor { get; set; }
+
+        [Required]
+        public int MaxNumbersOfBooking { get; set; }
+        
+        [Required]
+        public Room Room { get; set; }
+
+        [Required]
+        [ForeignKey("RoomId")]
+        public int RoomId { get; set; }
+
+        public ClassStatus Status { get; set; }
+
+        public virtual List<WaitingListEntry> WaitingList { get; set; }
+        public virtual List<Booking> Attendees { get; set; }
+        public virtual List<Equipment> Equpiments { get; set; }
+
+    }
+}
